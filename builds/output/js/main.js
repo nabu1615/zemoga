572dd7d9a926acffa99ebc89c6cd514a8955ad98
Javier Vargas <javvarga@cobogpgp1085.global.publicisgroupe.net>
2016-06-10 09:55:24 -0500

    refactoring js

--- builds/output/js/main.js
+++ builds/output/js/main.js
@@ -1,100 +1,9 @@
+var app = app || {};
 
-function hello() {
+app.lib = (function () {
+    var lib = {};
 
-    var menu = document.getElementsByClassName("nav-bar-toggle"),
-    subDrop = document.getElementsByClassName("drop-down-toogle"),
-    subNav = document.getElementsByClassName("nav-bar-subnav"),
-    nav = document.getElementsByClassName("nav-bar-list"),
-    header = document.getElementsByClassName("header"),
-    menuHeight = "-" + nav.clientHeight + "px";
-
-    window.addEventListener('resize', hideNavResize);
-
-    function hideNavResize() {
-        var w = window.innerWidth;
-        if(w > 768 ) {
-            nav[0].removeAttribute('style');
-        }
-    }
-
-    function showNav(item) {
-        if (item.style.display !== "inline-block") {
-            item.style.display = "inline-block";
-        }
-        else {
-            item.style.display = "";
-        }
-    };
-
-    menu[0].addEventListener('click', function() {showNav(nav[0]); } );
-    subDrop[0].addEventListener('click', function(e) {
-        e.preventDefault();
-        showNav(subNav[0]);
-    });
-
-};
-hello();
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var settings = {
-            "selectors": {
-                "slideVisible": "slide-visible",
-                "next": "next"
-            },
-            "classes": {
-                "slides": document.getElementsByClassName("slide-item"),
-                "slideVisible": document.getElementsByClassName("slide-visible"),
-                "next": document.getElementsByClassName("next"),
-                "slideButton": document.getElementsByClassName("slide-button")
-            }
-        },
-        slideLength = settings.classes.slides.length;
-
-    function initialize() {
-        for (var i = 0; i < settings.classes.slideButton.length; i++) {
-            settings.classes.slideButton[i].addEventListener('click', handleControlClick );
-        }
-    }
-
-    function handleControlClick(e) {
-        var activeIndex = getIndex(),
-            nextActiveIndex = getNextIndex(this, activeIndex);
-        setNextActiveSlide(nextActiveIndex);
-    }
-
-    function getIndex() {
-        var index = 0;
-        for (var i = 0; i < settings.classes.slides.length; i++) {
-            if (settings.classes.slides[i].classList.contains(settings.selectors.slideVisible)) {
-                index = i;
-                i = settings.classes.slides.length;
-            }
-        }
-        return index;
-    }
-
-
-    function getNextIndex(e, activeIndex) {
-        var isNext = e.classList.contains(settings.selectors.next);
-        if (isNext) {
-            return (activeIndex + 1 > slideLength - 1) ? 0 : activeIndex + 1;
-        } else {
-            return (activeIndex === 0) ? slideLength - 1 : activeIndex - 1;
-        }
-    }
-
-
-    function setNextActiveSlide(nextActiveIndex) {
-        var activeClass = settings.classes.slideVisible,
-            currentClass = activeClass[0],
-            slideId = document.getElementById('s-' + nextActiveIndex);
-        currentClass.classList.remove(settings.selectors.slideVisible);
-        fadeIn(slideId);
-        slideId.classList.add(settings.selectors.slideVisible);
-    }
-
-    function fadeIn(el) {
+    lib.fadeIn = function(el) {
       el.style.opacity = 0;
 
       var last = +new Date(),
@@ -110,9 +19,119 @@ document.addEventListener('DOMContentLoaded', function() {
       tick();
     }
 
+    lib.toggleVisibility = function(item) {
+        if (item.style.display !== "inline-block") {
+            item.style.display = "inline-block";
+        }
+        else {
+            item.style.display = "";
+        }
+    };
+
+    return lib;
+})();
+
+app.slide = (function () {
+        var settings;
+
+        function handleControlClick(e) {
+            var activeIndex = getIndex(),
+                nextActiveIndex = getNextIndex(this, activeIndex);
+            setNextActiveSlide(nextActiveIndex);
+        }
+
+        function getIndex() {
+            var index = 0;
+            for (var i = 0; i < settings.nodelist.slides.length; i++) {
+                if (settings.nodelist.slides[i].classList.contains(settings.classes.slideVisible)) {
+                    index = i;
+                    i = settings.nodelist.slides.length;
+                }
+            }
+            return index;
+        }
 
 
-    initialize();
+        function getNextIndex(e, activeIndex) {
+            var isNext = e.classList.contains(settings.classes.next);
+            if (isNext) {
+                return (activeIndex + 1 > slideLength - 1) ? 0 : activeIndex + 1;
+            } else {
+                return (activeIndex === 0) ? slideLength - 1 : activeIndex - 1;
+            }
+        }
+
+
+        function setNextActiveSlide(nextActiveIndex) {
+            var activeClass = settings.nodelist.slideVisible,
+                currentClass = activeClass[0],
+                slideId = document.getElementById('s-' + nextActiveIndex);
+            currentClass.classList.remove(settings.classes.slideVisible);
+            app.lib.fadeIn(slideId);
+            slideId.classList.add(settings.classes.slideVisible);
+        }
+
+        function initialize() {
+            settings = {
+                "classes": {
+                    "slideVisible": "slide-visible",
+                    "next": "next"
+                },
+                "nodelist": {
+                    "slides": document.getElementsByClassName("slide-item"),
+                    "slideVisible": document.getElementsByClassName("slide-visible"),
+                    "next": document.getElementsByClassName("next"),
+                    "slideButton": document.getElementsByClassName("slide-button")
+                }
+            }
+            slideLength = settings.nodelist.slides.length;
+
+            for (var i = 0; i < settings.nodelist.slideButton.length; i++) {
+                settings.nodelist.slideButton[i].addEventListener('click', handleControlClick );
+            }
+        }
+
+        return {
+            initialize: initialize
+        };
+})();
+
+app.nav = (function () {
+
+        var settings;
+
+        function hideNavResize() {
+            var w = window.innerWidth;
+            if(w > 768 ) {
+                settings.nodelist.nav[0].removeAttribute('style');
+            }
+        }
+
+        function initialize() {
+            settings = {
+                    "nodelist": {
+                        "menu": document.getElementsByClassName("nav-bar-toggle"),
+                        "subDrop": document.getElementsByClassName("drop-down-toogle"),
+                        "subNav": document.getElementsByClassName("nav-bar-subnav"),
+                        "nav": document.getElementsByClassName("nav-bar-list"),
+                        "header": document.getElementsByClassName("header"),
+                    }
+            };
+
+            window.addEventListener('resize', hideNavResize);
+            settings.nodelist.menu[0].addEventListener('click', function() {app.lib.toggleVisibility(settings.nodelist.nav[0]);} );
+            settings.nodelist.subDrop[0].addEventListener('click', function(e) {
+                e.preventDefault();
+                app.lib.toggleVisibility(settings.nodelist.subNav[0]);
+            });
+        }
+
+        return {
+            initialize: initialize
+        };
+})();
+
+document.addEventListener('DOMContentLoaded', function() {
+    app.nav.initialize();
+    app.slide.initialize();
 });
-
-
